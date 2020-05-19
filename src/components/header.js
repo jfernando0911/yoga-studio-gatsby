24b67ff261f0532e687c134ea893611feb11ade0
jfernando0911 <jfernando0911@gmail.com>
2020-05-19 15:40:56 -0500

    added links to redirect to parts of the page

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import styles from './header.module.css';
 import logo from '../LogoImage.svg';
-// import { Link } from "gatsby";
+import { Link } from "gatsby";
 
 const Header = () =>
     (
         <header className={styles.headerContainer}>
 
             <div className={styles.logoContainer}>
-                <img src={logo} alt="logo"></img>
+                <img src={logo} alt=""></img>
                 <div>Yoga studio</div>
             </div>
 
             <nav className={styles.navContainer}>
                 <ul className={styles.navUlContainer}>
-                    <li> Home</li>
-                    <li>Teachers</li>
-                    <li>Contact</li>
+                    <li> <Link>Home</Link></li>
+                    <li><Link to="#teachers">Teachers</Link></li>
+                    <li><Link to="#contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
